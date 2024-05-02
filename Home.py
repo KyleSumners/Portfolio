d736856c81fc65f09f9c7e2b8148785ba44f7a7c
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-05-02 01:03:25 -0500

    attempt fix 2

--- Home.py
+++ Home.py
@@ -32,7 +32,7 @@ with col3:
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+        st.write("[Source Code](" + row['url'] + ")")
 
 with col4:
     for index, row in df[1:].iterrows():
