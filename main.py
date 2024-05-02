d3b7bccecdaf92548b4a14d7b657b125218aa89f
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-05-01 22:13:05 -0500

    Add description on new columns

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ with col2:
     like to workout, read, play video games, and spend time with friends and family.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
